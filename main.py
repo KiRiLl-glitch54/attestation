2a5bc9010a9f2ad72ca63828616a617615c0f10b
KiRiLl-glitch54 <kukuninkirill2011@gmail.com>
2026-05-16 08:12:49 +0300

    Update main.py
    заготовка

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from models import Book
+from storage import add_book, delete_book, load_books
+from stats import get_average_rating, get_author_statistics
+import datetime
 
+def display_menu():
+    
+    print("\n--- Book Management Application ---")
+    print("1. Add New Book")
+    print("2. Delete Book")
+    print("3. View All Books")
+    print("4. View Statistics")
+    print("5. Exit")
+    print("-----------------------------------")
+
+def main():
+   pass
+if __name__ == "__main__":
+    main()
